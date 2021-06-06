4a6308359a75f175ad6d671c5acccb53f0751c04
ataastar <ata.astar@gmail.com>
2021-06-06 17:08:55 +0200

    interrogator schema is used

--- start.bat
+++ start.bat
@@ -1,7 +1,8 @@
 SET pw=%1
 
-SET PG_OPTIONS={"search_path":"interrogator"}
+REM public schema is used by default
+REM SET PG_OPTIONS={"search_path":"public"}
 SET PORT=3000
-SET DATABASE_URL=postgres://mata:%pw%@localhost:5432/postgres
+SET DATABASE_URL=postgres://mata:%pw%@localhost:5432/interrogator
 SET DATABASE_CONN_UNSECURE=true
 call node index.js
