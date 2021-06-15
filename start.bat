c87d97fe86f3e7aeee4f82dad3653654808820f1
ataastar <ata.astar@gmail.com>
2021-06-15 18:51:27 +0200

    INTERROGATOR_WEB env variable for enabled origin

--- start.bat
+++ start.bat
@@ -3,6 +3,7 @@ SET pw=%1
 REM public schema is used by default
 REM SET PG_OPTIONS={"search_path":"public"}
 SET INTERROGATOR_API_PORT=3000
+SET INTERROGATOR_WEB=http://localhost:4200
 SET DATABASE_URL=postgres://mata:%pw%@localhost:5432/interrogator
 rem SET INTERROGATOR_API_DB_CONN_UNSECURE=true
 call node index.js
