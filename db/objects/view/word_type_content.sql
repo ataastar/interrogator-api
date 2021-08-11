a221e09eb8683310af1e09d0d1e7cc31c6c813bc
ataastar <ata.astar@gmail.com>
2021-08-11 20:10:25 +0200

    word_type_content -> name, forms, select for active links

--- db/objects/view/word_type_content.sql
+++ db/objects/view/word_type_content.sql
@@ -43,7 +43,9 @@ CREATE OR REPLACE VIEW word_type_content(content, from_language_id, to_language_
                   JOIN phrase p_from ON wtf.phrase_id = p_from.phrase_id
                   JOIN phrase p_to ON wtfp.phrase_id = p_to.phrase_id;*/
 
-SELECT (SELECT row_to_json(from_phrase.*)
+SELECT (SELECT row_to_json(forms.*) AS forms FROM (SELECT array_to_json(array_agg(name)) AS forms FROM word_type_form wtfo WHERE wtfo.word_type_id = b_wt.word_type_id) forms)::jsonb ||
+       (SELECT row_to_json(wordtype.*) AS forms FROM (SELECT name AS name FROM word_type wt WHERE wt.word_type_id = b_wt.word_type_id) wordtype)::jsonb ||
+       (SELECT row_to_json(from_phrase.*)
         FROM (SELECT COALESCE(array_to_json(array_agg(phrase.*)), '[{}]'::json) as "rows"
               FROM (SELECT wtfr.word_type_link_id              id,
                            array_to_json(array_agg(p.text)) AS "fromPhrase",
@@ -52,9 +54,11 @@ SELECT (SELECT row_to_json(from_phrase.*)
                                   FROM word_type_form_phrase wtfp
                                            JOIN word_type_form wtf ON wtfp.word_type_form_id = wtf.word_type_form_id
                                            JOIN phrase p on wtfp.phrase_id = p.phrase_id
+                                           JOIN word_type_link wtl on wtl.word_type_link_id = wtfp.word_type_link_id
                                   WHERE wtfp.word_type_link_id = wtfr.word_type_link_id
                                         AND wtf.word_type_id = b_wt.word_type_id
                                         AND p.language_id = b_wt.language_id
+                                        AND wtl.active
                                   GROUP BY wtf.name) t
                             ) to_phrases
                     FROM word_type_from wtfr
@@ -62,7 +66,7 @@ SELECT (SELECT row_to_json(from_phrase.*)
                     WHERE p.language_id = b_l.language_id
                     GROUP BY wtfr.word_type_link_id) phrase
              ) AS from_phrase
-       ) AS content,
+       )::jsonb AS content ,
     b_l.language_id from_languge_id, b_wt.language_id to_languge_id, b_wt.word_type_id
 FROM word_type b_wt
          CROSS JOIN language b_l;
