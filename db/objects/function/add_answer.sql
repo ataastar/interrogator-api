0a04e0697c25a69379d8b1cea4101c118a156c9f
ataastar <ata.astar@gmail.com>
2021-11-30 22:21:59 +0100

    add_answer function

--- db/objects/function/add_answer.sql
+++ db/objects/function/add_answer.sql
@@ -1,7 +1,7 @@
-CREATE OR REPLACE PROCEDURE add_answer(p_unit_content_id bigint, p_user_id bigint, p_answer_is_right boolean,
-                                       p_interrogator_type text)
-  language plpgsql
-as
+DROP FUNCTION IF EXISTS add_answer;
+CREATE OR REPLACE FUNCTION add_answer(p_unit_content_id BIGINT, p_user_id BIGINT, p_answer_is_right BOOLEAN, p_interrogator_type TEXT) RETURNS BOOLEAN
+  LANGUAGE plpgsql
+AS
 $$
 BEGIN
   INSERT INTO answer(translation_link_id, user_id, from_language_id, right_answer, interrogation_type)
@@ -9,13 +9,12 @@ BEGIN
          p_user_id,
          (SELECT MAX(p.language_id)
           FROM translation_from tf
-                 JOIN phrase p on tf.phrase_id = p.phrase_id
+                 JOIN phrase p ON tf.phrase_id = p.phrase_id
           WHERE uc.translation_link_id = tf.translation_link_id),
          p_answer_is_right,
          p_interrogator_type
   FROM unit_content uc
   WHERE uc.unit_content_id = p_unit_content_id;
+  RETURN TRUE;
 END
 $$;
-
-
