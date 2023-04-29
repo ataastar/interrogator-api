22a6ccec7ab180dbd78905ebf376d2fbc2b6e636
Molnar Attila <ata.astar@gmail.com>
2023-04-29 21:01:07 +0200

    word_types/units/words endpoint

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post('/answer', auth.checkIfAuthenticated, (req, res) => {
 
 // word types e.g. Irregular verbs
 app.get('/word_types/words/:wordTypeId/:fromLanguageId/:toLanguageId', auth.checkIfAuthenticated, phrase.getWordTypeContent)
-app.get('/word_types/unit/words/:wordTypeUnitId/:fromLanguageId', auth.checkIfAuthenticated, phrase.getWordTypeUnitContent)
+app.get('/word_types/units/words/:wordTypeUnitId/:fromLanguageId', auth.checkIfAuthenticated, phrase.getWordTypeUnitContent)
 app.get('/word_type_unit', auth.checkIfAuthenticated, phrase.getWordTypeUnit)
 // adding removing phrases
 app.put('/word_types/units/link', auth.checkIfAuthenticated, phrase.addWordTypeUnitLink)
