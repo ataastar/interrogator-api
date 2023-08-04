ebdd5a7e67e3b44e136b0652b11fa3a317ab9172
Molnar Attila <ata.astar@gmail.com>
2023-08-04 20:27:41 +0200

    db test fixes

--- db/test/test_call_all.sql
+++ db/test/test_call_all.sql
@@ -2,4 +2,5 @@
 call test_clean_all();
 call test_base_script();
 
+call test_create_language();
 call test_unit_next_interrogation_date();
--- db/test/test_clean_all.sql
+++ db/test/test_clean_all.sql
@@ -4,5 +4,24 @@ AS
 $$
 BEGIN
   TRUNCATE TABLE interrogation_interval;
+  truncate table answer;
+  truncate table log;
+  truncate table translation;
+  truncate table unit_content;
+  truncate table unit_tree cascade;
+
+  truncate table word_type_form_phrase cascade;
+  truncate table word_type_from;
+  truncate table word_type_unit_link;
+  truncate table word_type_form cascade;
+  truncate table word_type cascade;
+  truncate table word_type_unit cascade;
+
+  truncate table phrase cascade;
+  truncate table language cascade;
+  truncate table users;
+  truncate table user_role;
+
+  truncate table translation_link cascade;
 END;
 $$
